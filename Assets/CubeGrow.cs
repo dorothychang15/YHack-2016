743f390864b686ae7e942cb9f82487f6c0956d4d
Dorothy Chang <dorothyichang@gmail.com>
2016-11-12 18:56:07 -0500

    display data

--- Assets/CubeGrow.cs
+++ Assets/CubeGrow.cs
@@ -1,17 +1,41 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class CubeGrow : MonoBehaviour {
 
+	public GameObject us;
+	public GameObject china;
+	public Dictionary<string, int> data;
+	public Dictionary<string, GameObject> map;
 
 	// Use this for initialization
 	void Start () {
+		// read in input data
+		// determine values for each
+		data = new Dictionary<string, int>();
+		data.Add ("us", 5);
+		data.Add ("china", 10);
+		map = new Dictionary<string, GameObject> ();
+		map.Add ("us", us);
+		map.Add ("china", china);
+		draw ();
 	}
 
-	// Update is called once per frame
-	void Update () {
-		transform.localScale += new Vector3 (0, 0.01f, 0);
-		Vector3 position = transform.position;
-		transform.position = new Vector3 (position.x, 0, position.z);
+	public void draw () {
+		foreach(KeyValuePair<string, int> entry in data)
+		{
+			GameObject obj = map [entry.Key];
+			for (int i = 0; i < entry.Value; i++) {
+				grow (obj);
+			}
+		}
 	}
+
+	public void grow (GameObject obj) {
+		obj.transform.localScale += new Vector3 (0, 0.1f, 0);
+		Vector3 position = obj.transform.position;
+		obj.transform.position = new Vector3 (position.x, 0, position.z);
+	}
+
+
 }
