b702cfe6da8b5a77d10a6f35c105ea138bb46084
Dorothy Chang <dorothyichang@gmail.com>
2016-11-12 16:43:36 -0500

    cube grow implemented

---
+++ Assets/CubeGrow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeGrow : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.localScale += new Vector3 (0, 0.01f, 0);
+		Vector3 position = transform.position;
+		transform.position = new Vector3 (position.x, 0, position.z);
+	}
+}
